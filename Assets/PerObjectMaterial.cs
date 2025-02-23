10aaad2da8b50dc393000cfbc46e6353ceb6956f
jace <jace2203@gmail.com>
2025-02-23 23:45:34 +0800

    Continue with 3.5 Reflectivity

--- Assets/PerObjectMaterial.cs
+++ Assets/PerObjectMaterial.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class PerObjectMaterial : MonoBehaviour
 {
-    private static int baseColorId = Shader.PropertyToID("_BaseColor");
-    private static int cutoffId = Shader.PropertyToID("_Cutoff");
+    private static int 
+        baseColorId = Shader.PropertyToID("_BaseColor"),
+        cutoffId = Shader.PropertyToID("_Cutoff"),
+        metallicId = Shader.PropertyToID("_Metallic"),
+        smoothnessId = Shader.PropertyToID("_Smoothness");
+
     private static MaterialPropertyBlock block;
 
     [SerializeField]
     private Color baseColor = Color.white;
     [SerializeField, Range(0f, 1f)]
-    private float cutoff = 0.5f;
+    private float
+        cutoff = 0.5f,
+        metallic = 0f,
+        smoothness = 0.5f;
 
     void Awake()
     {
@@ -24,6 +31,8 @@ public class PerObjectMaterial : MonoBehaviour
         if (block == null) block = new MaterialPropertyBlock();
         block.SetColor(baseColorId, baseColor);
         block.SetFloat(cutoffId, cutoff);
+        block.SetFloat(metallicId, metallic);
+        block.SetFloat(smoothnessId, smoothness);
         GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
